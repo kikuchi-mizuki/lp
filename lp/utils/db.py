25f0dca3b618e3519cd2e6fb2fceb6736e0001d5
kikuchi-mizuki <kikuchimizuki@kikuchimizukinonotopasokon.local>
2025-07-25 17:26:37 +0900

    DBファイルの絶対パスをprintして本番環境のパス問題を調査

--- lp/utils/db.py
+++ lp/utils/db.py
@@ -5,6 +5,7 @@ import psycopg2
 DATABASE_URL = os.getenv('DATABASE_URL', 'database.db')
 
 def get_db_connection():
+    print('DBファイルのパス:', os.path.abspath(DATABASE_URL))
     if DATABASE_URL.startswith('postgresql://'):
         return psycopg2.connect(DATABASE_URL)
     else:
