10168f03e764831d507111473713936b0dd44dbb
kikuchi-mizuki <kikuchimizuki@kikuchimizukinonotopasokon.local>
2025-08-08 19:43:20 +0900

    Create minimal app to break deployment cycle

---
+++ lp/app_minimal.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def health_check():
+    return "OK", 200
+
+@app.route('/health')
+def health():
+    return "OK", 200
+
+@app.route('/ping')
+def ping():
+    return "pong", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
